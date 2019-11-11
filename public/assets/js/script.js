d98effa372828dbc73443eaeb67deca096dfb308
JSutliff <sutliff.jason@gmail.com>
2019-11-11 13:42:42 -0800

    advertise page styled

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -7,3 +7,13 @@ const closeSlideMenu = () => {
   document.getElementById("side-menu").style.width = "0px";
   document.getElementById("tagline").style.marginLeft = "0px";
 };
+
+const openModal = () => {
+  const modal = document.getElementById("simpleModal");
+  modal.style.display = "block";
+};
+
+const closeModal = () => {
+  const modal = document.getElementById("simpleModal");
+  modal.style.display = "none";
+};
